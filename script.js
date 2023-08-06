aa990836e86f29caaa8df22ee28ba174460c4fa8
nadirshax <07271993@gmail.com>
2023-08-06 19:51:24 +0500

    JavaScript-1

---
+++ script.js
@@ -0,0 +1,19 @@
+// let person = {
+//     name: 'Nadirshax',
+//     age: 30,
+//     isMaerried: true,
+// }
+// console.log(person);
+
+
+// let arr = ['telefon', 'Nadirshax', 'Shadiyev']
+// console.log(arr);
+// arr[0] = "Phone"
+// console.log(arr);
+
+// alert('Salom dunyo')
+// let answers = confirm('Sizi yoshiz 18 dami')
+// console.log(answers);
+
+let answers = prompt('Sizi ismiz kim')
+console.log(answers);
